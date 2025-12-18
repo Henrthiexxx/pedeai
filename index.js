34ecaddfcb67bc6a7e65ce6af6b2664d5d940519
Henrthiexxx <deliverylino@gmail.com>
2025-12-17 20:18:00 -0400

    Update Firebase storage bucket URL

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const firebaseConfig = {
     apiKey: "AIzaSyAnIJRcUxN-0swpVnonPbJjTSK87o4CQ_g",
     authDomain: "pedrad-814d0.firebaseapp.com",
     projectId: "pedrad-814d0",
-    storageBucket: "pedrad-814d0.firebasestorage.app",
+    storageBucket: "pedrad-814d0.appspot.com",
+
     messagingSenderId: "293587190550",
     appId: "1:293587190550:web:80c9399f82847c80e20637"
 };
